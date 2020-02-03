5509dc3ab42fbba496e4bfc583f3d0ea7d59a876
Tom Brus <tombrus@gmail.com>
2020-02-03 23:20:40 +0100

    initial push

---
+++ README.md
@@ -0,0 +1,31 @@
+# sync-s3-action
+```get``` or ```put``` files from/to an S3 bucket
+
+The Amsterdam Scaleway S3 repo is the default but you can use other Scaleway and AWS as well.
+
+## Example Usage
+
+To put some files in S3:
+```yaml
+      - name: "put in S3"
+        uses: ModelingValueGroup/sync-s3-action@master
+        with:
+          access_key: "${{ secrets.SCALEWAY_ACC }}"
+          secret_key: "${{ secrets.SCALEWAY_SEC }}"
+          bucket: my-bucket
+          cmd: put
+          local_dir: theDirOnDisk
+          s3_dir: theDirInS3
+```
+To get thhis file back:
+```yaml
+      - name: "get from S3"
+        uses: ModelingValueGroup/sync-s3-action@master
+        with:
+          access_key: "${{ secrets.SCALEWAY_ACC }}"
+          secret_key: "${{ secrets.SCALEWAY_SEC }}"
+          bucket: my-bucket
+          cmd: get
+          local_dir: theDirOnDiskAgain
+          s3_dir: theDirInS3
+```
---
+++ entrypoint.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## (C) Copyright 2018-2019 Modeling Value Group B.V. (http://modelingvalue.org)                                        ~
+##                                                                                                                     ~
+## Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in      ~
+## compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
+## Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
+## an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+## specific language governing permissions and limitations under the License.                                          ~
+##                                                                                                                     ~
+## Maintainers:                                                                                                        ~
+##     Wim Bast, Tom Brus, Ronald Krijgsheld                                                                           ~
+## Contributors:                                                                                                       ~
+##     Arjan Kok, Carel Bast                                                                                           ~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+set -euo pipefail
+
+if [[ "$INPUT_TRACE" == "true" ]]; then
+    set -x
+fi
+
+# INPUT_HOST
+# INPUT_REGION
+# INPUT_BUCKET
+# INPUT_ACCESS_KEY
+# INPUT_SECRET_KEY
+# INPUT_CMD         = (put|get)
+# INPUT_LOCAL_DIR
+# INPUT_S3_DIR
+
+if ! command -v s3cmd >/dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y s3cmd
+fi
+
+if [[ "$INPUT_HOST" == "" ]];then
+    if [[ "$INPUT_REGION" == "" ]]; then
+        INPUT_REGION="nl-ams"
+    fi
+    INPUT_HOST="https://s3.$INPUT_REGION.scw.cloud"
+fi
+if [[ "$INPUT_CMD" != "put" && "$INPUT_CMD" != "get" ]]; then
+    echo "::error::CMD must be 'put' or 'get' (not '$INPUT_CMD')"
+    exit 99
+fi
+
+echo "# going to $INPUT_CMD on/from $INPUT_HOST"
+
+s3cmd_(){
+    s3cmd                                           \
+                   --host="https://$INPUT_HOST"     \
+            --host-bucket=                          \
+             --access_key="$INPUT_ACCESS_KEY"       \
+             --secret_key="$INPUT_SECRET_KEY"       \
+        "$@"
+}
+
+if ! s3cmd_ -q ls "s3://$INPUT_BUCKET"; then
+    echo "# creating bucket: $INPUT_BUCKET"
+    s3cmd_  mb "s3://$INPUT_BUCKET"
+fi
+case "$INPUT_CMD" in
+(put)
+    s3cmd_ --recursive put "$INPUT_LOCAL_DIR/" "s3://$INPUT_BUCKET/$INPUT_S3_DIR/"
+    ;;
+(get)
+    mkdir -p "$INPUT_LOCAL_DIR"
+    s3cmd_ --recursive get "s3://$INPUT_BUCKET/$INPUT_S3_DIR/" "$INPUT_LOCAL_DIR/"
+    ;;
+esac
