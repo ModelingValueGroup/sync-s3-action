9665d1bef9a23b66828640c3ee9878f1f4801d51
Tom Brus <tombrus@gmail.com>
2020-02-09 16:00:43 +0100

    check if there are trigger files for other actions

--- functions.sh
+++ functions.sh
@@ -111,7 +111,7 @@ trigger() {
     if [[ "${INPUT_S3_DIR_BRANCHED:-}" != "" ]]; then
         if [[ "$(s3cmd_ ls "$to$TRIGGERS_DIR/" | wc -l)" != 0 ]]; then
             local triggersTmpDir="$TRIGGERS_DIR-$$"
-            s3cmd_ get "$to$TRIGGERS_DIR/" "$triggersTmpDir"
+            s3cmd_ --recursive get "$to$TRIGGERS_DIR/" "$triggersTmpDir"
             local f
             for f in $triggersTmpDir/*.trigger; do
                 if [[ -f "$f" ]]; then
